ae19becc6237085fe370f2d7daf0079a853e52c8
justalexok <76539791+justalexok@users.noreply.github.com>
2024-01-05 13:13:53 +0000

    Add Fluff Prog Bar

---
+++ Source/FluffquakeFury/Public/AbilitySystem/Abilities/FQFGameplayAbility.h
@@ -0,0 +1,17 @@
+// Copyright Alex Goulder
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "FQFGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLUFFQUAKEFURY_API UFQFGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
